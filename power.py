ac29ce40149268a7aec218c933ea60f7ceb1c540
allan almax <allanalmax13@gmail.com>
2019-02-19 19:37:17 +0300

    this is a recursion program

---
+++ power.py
@@ -0,0 +1,9 @@
+def power(number, power_to_raise):
+    if power_to_raise == 0:
+        return 1
+    else:
+        power_to_raise = power_to_raise - 1
+        return number * power(number, power_to_raise)
+
+result = power(2, 4)
+print(result)
